82bc1994b95931cb6583c1027cbbaadaa67b4c7a
nopnop2002 <hau26280@tree.odn.ne.jp>
2024-07-06 09:51:44 +0900

    added argparse

--- tcp.py
+++ tcp.py
@@ -1,34 +1,36 @@
 #!/usr/bin/python
 #-*- encoding: utf-8 -*-
+import argparse
 import socket
 import signal
 
-global TaskRunning
-
 def handler(signal, frame):
-	global TaskRunning
+	global running
 	print('CNTL-C')
-	TaskRunning = False
+	running = False
 
+if __name__ == '__main__':
+	signal.signal(signal.SIGINT, handler)
+	running = True
 
-host = "esp32-server.local" # mDNS hostname
-port = 5000
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--host', help="TCP Server", default="esp32-server.local")
+	parser.add_argument('--port', type=int, help="TCP Port", default=5000)
+	args = parser.parse_args()
+	print("args.host={}".format(args.host))
+	print("args.port={}".format(args.port))
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	client.connect((args.host, args.port))
+	client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-client.connect((host, port))
-
-client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-TaskRunning = True
-signal.signal(signal.SIGINT, handler)
-
-while(TaskRunning):
-	msg = client.recv(1024)
-	#print("type(msg)={}".format(type(msg)))
-	if (type(msg) is bytes):
-		msg=msg.decode('utf-8')
+	while running:
+		msg = client.recv(1024)
+		#print("type(msg)={}".format(type(msg)))
+		if (type(msg) is bytes):
+			msg=msg.decode('utf-8')
 		print(msg.strip())
 
-client.close()
+	#print("socket close")
+	client.close()
 
--- udp.py
+++ udp.py
@@ -1,13 +1,34 @@
+#!/usr/bin/python
+#-*- encoding: utf-8 -*-
+import argparse
 import select, socket
+import signal
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('<broadcast>', 9000))
-s.setblocking(0)
+def handler(signal, frame):
+	global running
+	print('handler')
+	running = False
 
-while True:
-	result = select.select([s],[],[])
-	msg = result[0][0].recv(1024)
-	#print("type(msg)={}".format(type(msg)))
-	if (type(msg) is bytes):
-		msg=msg.decode('utf-8')
-	print(msg.strip())
+if __name__ == '__main__':
+	signal.signal(signal.SIGINT, handler)
+	running = True
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--port', type=int, help="UDP Port", default=9000)
+	args = parser.parse_args()
+	print("args.port={}".format(args.port))
+
+	client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	client.bind(('<broadcast>', args.port))
+	client.setblocking(0)
+
+	while running:
+		result = select.select([client],[],[])
+		msg = result[0][0].recv(1024)
+		#print("type(msg)={}".format(type(msg)))
+		if (type(msg) is bytes):
+			msg=msg.decode('utf-8')
+		print(msg.strip())
+
+	#print("socket close")
+	client.close()
